e9901145a8b7bfa6bb9d0c14dae4af56b7ebb03a
Calla-la <v-capan@microsoft.com>
2026-08-30 10:52:39 +0800

    Add CMMR-RT update script

---
+++ scripts/Update-CmmrRt.ps1
@@ -0,0 +1,9 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$WorkbookPath
+)
+
+$ErrorActionPreference = 'Stop'
+
+Write-Host "CMMR-RT update script is not implemented yet."
+Write-Host "Workbook: $WorkbookPath"
